b489c7fcce95b7c1dc6358f3af8fe162e9c76090
tsokkisl <tsokkis.l@gmail.com>
2022-08-24 00:55:58 +0300

    Add proof of work

--- main.go
+++ main.go
@@ -9,11 +9,12 @@ import (
 )
 
 type Blockchain struct {
-	blocks []*Block
+	blocks     []*Block
+	difficulty int
 }
 
 type Block struct {
-	nonce    []byte
+	nonce    int
 	hash     []byte
 	data     [][]byte
 	prevHash []byte
@@ -37,38 +38,51 @@ func serializeData(d [][]byte) []byte {
 }
 
 func (b *Block) generateHash() {
-	hash := sha256.Sum256(bytes.Join([][]byte{b.nonce, serializeData(b.data), b.prevHash}, []byte{}))
+	hash := sha256.Sum256(bytes.Join([][]byte{[]byte(strconv.Itoa(b.nonce)), serializeData(b.data), b.prevHash}, []byte{}))
 	b.hash = hash[:]
 }
 
 func (bc *Blockchain) printBlockchain() {
 	for i, block := range bc.blocks {
-		fmt.Printf("\nBlock %d\n-----\nHash: %x\nData: %s\nPrev Hash: %x\nNonce: %s\n", i, block.hash, block.data, block.prevHash, block.nonce)
+		fmt.Printf("\nBlock %d\n-----\nHash: %x\nData: %s\nPrev Hash: %x\nNonce: %d\n", i, block.hash, block.data, block.prevHash, block.nonce)
 	}
 }
 
 func (bc *Blockchain) addBlock(b *Block) {
+	proofOfWork := false
+	diff := fmt.Sprintf("%0*d", bc.difficulty, 0)
+	hashPrefix := string(fmt.Sprintf("%x", b.hash))[:bc.difficulty]
+	for proofOfWork != true {
+		if hashPrefix == diff {
+			proofOfWork = true
+		} else {
+			b.nonce += 1
+			hash := sha256.Sum256(bytes.Join([][]byte{[]byte(strconv.Itoa(b.nonce)), serializeData(b.data), b.prevHash}, []byte{}))
+			b.hash = hash[:]
+			hashPrefix = string(fmt.Sprintf("%x", b.hash))[:bc.difficulty]
+		}
+	}
 	bc.blocks = append(bc.blocks, b)
 }
 
 func (bc *Blockchain) generateBlock() *Block {
-	var block = Block{[]byte(strconv.Itoa(100000000 + rand.Intn(100000000))), []byte{}, [][]byte{[]byte(randString(20)), []byte(randString(20))}, bc.blocks[len(bc.blocks)-1].hash}
+	var block = Block{100000000 + rand.Intn(100000000), []byte{}, [][]byte{[]byte(randString(20)), []byte(randString(20))}, bc.blocks[len(bc.blocks)-1].hash}
 	block.generateHash()
 	return &block
 }
 
-func initBlockchain() *Blockchain {
-	var bc = Blockchain{[]*Block{}}
-	var genesisBlock = Block{[]byte(strconv.Itoa(100000000 + rand.Intn(100000000))), []byte{}, [][]byte{[]byte("Genesis"), []byte("Block")}, []byte{}}
+func initBlockchain(difficulty int) *Blockchain {
+	var bc = Blockchain{[]*Block{}, difficulty}
+	var genesisBlock = Block{100000000 + rand.Intn(100000000), []byte{}, [][]byte{[]byte("Genesis"), []byte("Block")}, []byte{}}
 	genesisBlock.generateHash()
 	bc.addBlock(&genesisBlock)
 	return &bc
 }
 
 func main() {
-	blockChain := initBlockchain()
-	i := 0
-	for i != 10 {
+	difficulty := 5
+	blockChain := initBlockchain(difficulty)
+	for i := 0; i != 5; {
 		block := blockChain.generateBlock()
 		blockChain.addBlock(block)
 		i++
